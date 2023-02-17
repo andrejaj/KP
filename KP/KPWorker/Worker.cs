5b54b082a434664c8b8afe4afe549cefd9035676
Andreja Jovanovic <ajovanov@hotmail.co.uk>
2023-02-17 12:34:55 +0100

    Added commented code to sleep for a day before activating again.

--- KP/KPWorker/Worker.cs
+++ KP/KPWorker/Worker.cs
@@ -18,18 +18,28 @@ namespace KPWorker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                _logger.LogDebug("Worker started at: {time}", DateTimeOffset.Now);
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogDebug("Worker started at: {time}", DateTimeOffset.Now);
 
-                _dataScraper.LoadData();
+                    _dataScraper.LoadData();
 
-                _logger.LogDebug("Worker finished at: {time}", DateTimeOffset.Now);
+                    _logger.LogDebug("Worker finished at: {time}", DateTimeOffset.Now);
 
-                await Task.Delay(TimeSpan.FromSeconds(DelayInDays), stoppingToken);
+                    await Task.Delay(TimeSpan.FromSeconds(DelayInDays) /*TimeSpan.FromDays(DelayInDays)*/, stoppingToken);
 
-                // When completed, the entire app host will stop.
-                //_lifetime.StopApplication();
+                    // When completed, the entire app host will stop.
+                    //_lifetime.StopApplication();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "{Message}", ex.Message);
+
+                // Terminates this process and returns an exit code to the operating system.
+                Environment.Exit(1);
             }
         }
     }
